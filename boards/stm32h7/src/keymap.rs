353d85b9b89b0e89cc01e6e4861bc745ee4966ec
Haobo Gu <haobogu@outlook.com>
2023-09-21 21:14:00 +0800

    fix(stm32): fix debugging config
    Signed-off-by: Haobo Gu <haobogu@outlook.com>

--- boards/stm32h7/src/keymap.rs
+++ boards/stm32h7/src/keymap.rs
@@ -9,13 +9,13 @@ pub static KEYMAP: [[[KeyAction; COL]; ROW]; NUM_LAYER] = [
     layer!([
         [k!(A), k!(B), k!(C)],
         [k!(Kp4), k!(LShift), k!(Kp6)],
-        [k!(Kp1), k!(Kp2), k!(Kp3)],
+        [mo!(1), k!(Kp2), k!(Kp3)],
         [mo!(1), a!(No), k!(Kp0)]
     ]),
     layer!([
         [k!(Kp7), k!(Kp8), k!(Kp9)],
         [k!(Kp4), k!(LCtrl), k!(Kp6)],
-        [k!(Kp1), k!(Kp2), k!(Kp3)],
+        [mo!(1), k!(Kp2), k!(Kp3)],
         [mo!(1), a!(No), k!(Kp0)]
     ]),
 ];
