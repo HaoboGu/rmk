dbff62aa379d4c76822869f85a4cb12949378832
Haobo Gu <haobogu@outlook.com>
2023-10-06 22:20:24 +0800

    feat(via): use `byteoder` to process via bytes
    Signed-off-by: Haobo Gu <haobogu@outlook.com>

--- rmk/src/via/process.rs
+++ rmk/src/via/process.rs
@@ -1,4 +1,5 @@
-use super::{protocol::*, descriptor::*, *};
+use super::{descriptor::*, protocol::*, *};
+use byteorder::{BigEndian, ByteOrder};
 use log::info;
 use num_enum::{FromPrimitive, TryFromPrimitive};
 use rtic_monotonics::{systick::Systick, Monotonic};
@@ -14,7 +15,7 @@ pub fn process_via_packet(report: &mut ViaReport) {
     report.input_data = report.output_data;
     match ViaCommand::from_primitive(command_id) {
         ViaCommand::GetProtocolVersion => {
-            set_data_u16(&mut report.input_data[1..], VIA_PROTOCOL_VERSION);
+            BigEndian::write_u16(&mut report.input_data[1..3], VIA_PROTOCOL_VERSION);
         }
         ViaCommand::GetKeyboardValue => {
             // Check the second u8
@@ -22,15 +23,19 @@ pub fn process_via_packet(report: &mut ViaReport) {
                 Ok(v) => match v {
                     ViaKeyboardInfo::Uptime => {
                         let value = Systick::now().ticks();
-                        set_data_u32(&mut report.input_data[1..], value);
+                        BigEndian::write_u32(&mut report.input_data[2..6], value);
                     }
-                    ViaKeyboardInfo::LayoutOptions => todo!("GetKeyboardValue - LayoutOptions: need eeprom"),
-                    ViaKeyboardInfo::SwitchMatrixState => todo!("GetKeyboardValue - SwitchMatrixState"),
+                    ViaKeyboardInfo::LayoutOptions => {
+                        let _layout_option: u32 = todo!("get layout option");
+                        // BigEndian::write_u32(&mut report.input_data[2..6], _layout_option);
+                    }
+                    ViaKeyboardInfo::SwitchMatrixState => {
+                        todo!("GetKeyboardValue - SwitchMatrixState")
+                    }
                     ViaKeyboardInfo::FirmwareVersion => {
-                        let value = VIA_FIRMWARE_VERSION;
-                        set_data_u32(&mut report.input_data[1..], value);
+                        BigEndian::write_u32(&mut report.input_data[2..6], VIA_FIRMWARE_VERSION);
                     }
-                    _ => ()
+                    _ => (),
                 },
                 Err(e) => error!("Invalid subcommand: {} of GetKeyboardValue", e.number),
             }
@@ -39,15 +44,33 @@ pub fn process_via_packet(report: &mut ViaReport) {
             // Check the second u8
             match ViaKeyboardInfo::try_from_primitive(report.output_data[1]) {
                 Ok(v) => match v {
-                    ViaKeyboardInfo::LayoutOptions => todo!("SetKeyboardValue - LayoutOptions: need eeprom"),
-                    ViaKeyboardInfo::DeviceIndication => todo!("SetKeyboardValue - DeviceIndication"),
-                    _ => ()
+                    ViaKeyboardInfo::LayoutOptions => {
+                        let _layout_option = BigEndian::read_u32(&report.output_data[2..6]);
+                        todo!("SetKeyboardValue - LayoutOptions: need eeprom");
+                    }
+                    ViaKeyboardInfo::DeviceIndication => {
+                        let _device_indication = report.output_data[2];
+                        todo!("SetKeyboardValue - DeviceIndication")
+                    }
+                    _ => (),
                 },
                 Err(e) => error!("Invalid subcommand: {} of GetKeyboardValue", e.number),
             }
         }
-        ViaCommand::DynamicKeymapGetKeycode => todo!(),
-        ViaCommand::DynamicKeymapSetKeycode => todo!(),
+        ViaCommand::DynamicKeymapGetKeycode => {
+            let _layer = report.output_data[1];
+            let _row = report.output_data[2];
+            let _col = report.output_data[3];
+            let _keycode: u16 = todo!("get keycode");
+            // BigEndian::write_u16(&mut report.input_data[4..6], keycode);
+        }
+        ViaCommand::DynamicKeymapSetKeycode => {
+            let _layer = report.output_data[1];
+            let _row = report.output_data[2];
+            let _col = report.output_data[3]; 
+            let _keycode = BigEndian::read_u16(&report.output_data[4..6]);
+            todo!("DynamicKeymap - Set Keycode")
+        }
         ViaCommand::DynamicKeymapReset => todo!(),
         ViaCommand::CustomSetValue => todo!(),
         ViaCommand::CustomGetValue => todo!(),
@@ -67,16 +90,4 @@ pub fn process_via_packet(report: &mut ViaReport) {
         ViaCommand::Vial => info!("Received vial command!"),
         ViaCommand::Unhandled => todo!(),
     }
-}
-
-fn set_data_u32(data: &mut [u8], value: u32) {
-    data[0] = ((value >> 24) as u8) & 0xFF;
-    data[1] = ((value >> 16) as u8) & 0xFF;
-    data[2] = ((value >> 8) as u8) & 0xFF;
-    data[3] = (value as u8) & 0xFF;
-}
-
-fn set_data_u16(data: &mut [u8], value: u16) {
-    data[0] = ((value >> 8) as u8) & 0xFF;
-    data[1] = (value as u8) & 0xFF;
 }
