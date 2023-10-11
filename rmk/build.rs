285b722f82f6bdd6355a91d7384b7264d2fee0bd
Haobo Gu <haobogu@outlook.com>
2023-10-11 21:54:29 +0800

    chore(proj): don't bind to any linker script in rmk core
    Signed-off-by: Haobo Gu <haobogu@outlook.com>

--- rmk/build.rs
+++ rmk/build.rs
@@ -19,20 +19,12 @@ fn main() {
     // Put `memory.x` in our output directory and ensure it's
     // on the linker search path.
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    File::create(out.join("memory.x"))
-        .unwrap()
-        .write_all(include_bytes!("memory.x"))
-        .unwrap();
-    println!("cargo:rustc-link-search={}", out.display());
 
-    // By default, Cargo will re-run a build script whenever
-    // any file in the project changes. By specifying `memory.x`
-    // here, we ensure the build script is only re-run when
-    // `memory.x` is changed.
-    println!("cargo:rerun-if-changed=memory.x");
+    println!("cargo:rustc-link-search={}", out.display());
 
     // Specify linker arguments.
     let target = std::env::var("TARGET").unwrap();
+
     // For testing in std, don't set link arg 
     if target.contains("eabi") {
         // `--nmagic` is required if memory section addresses are not aligned to 0x10000,
