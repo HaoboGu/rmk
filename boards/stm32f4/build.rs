996ffef27e48c256988fd41905486e99964ea95d
Haobo Gu <haobogu@outlook.com>
2024-04-18 21:27:25 +0800

    refactor: use `memory-x` for stm32 chips
    Signed-off-by: Haobo Gu <haobogu@outlook.com>

--- boards/stm32f4/build.rs
+++ boards/stm32f4/build.rs
@@ -12,29 +12,14 @@
 
 use const_gen::*;
 use std::fs::File;
-use std::io::{Read, Write};
-use std::path::{Path, PathBuf};
+use std::io::Read;
+use std::path::Path;
 use std::{env, fs};
 use xz2::read::XzEncoder;
 
 fn main() {
     // Generate vial config at the root of project
     generate_vial_config();
-
-    // Put `memory.x` in our output directory and ensure it's
-    // on the linker search path.
-    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    File::create(out.join("memory.x"))
-        .unwrap()
-        .write_all(include_bytes!("memory.x"))
-        .unwrap();
-    println!("cargo:rustc-link-search={}", out.display());
-
-    // By default, Cargo will re-run a build script whenever
-    // any file in the project changes. By specifying `memory.x`
-    // here, we ensure the build script is only re-run when
-    // `memory.x` is changed.
-    println!("cargo:rerun-if-changed=memory.x");
 
     // Specify linker arguments.
 
