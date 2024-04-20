34364bd91143274908f59789a4287e7ff47d6d4e
Haobo Gu <haobogu@outlook.com>
2024-04-21 01:06:16 +0800

    feat(example): add example config for stm32f407ve
    Signed-off-by: Haobo Gu <haobogu@outlook.com>

--- boards/stm32f4/src/main.rs
+++ boards/stm32f4/src/main.rs
@@ -32,16 +32,20 @@ bind_interrupts!(struct Irqs {
 async fn main(_spawner: Spawner) {
     info!("RMK start!");
     // RCC config
+    // Tested on stm32f411 with 25MHZ HSE and stm32f407 with 8MHZ HSE
+    // The commented rcc configuration code is for stm32f407 
     let mut config = Config::default();
     {
         use embassy_stm32::rcc::*;
         config.rcc.hse = Some(Hse {
             freq: Hertz(25_000_000),
+            // freq: Hertz(8_000_000),
             mode: HseMode::Oscillator,
         });
         config.rcc.pll_src = PllSource::HSE;
         config.rcc.pll = Some(Pll {
             prediv: PllPreDiv::DIV25,
+            // prediv: PllPreDiv::DIV8,
             mul: PllMul::MUL192,
             divp: Some(PllPDiv::DIV2), // 25mhz / 25 * 192 / 2 = 96Mhz.
             divq: Some(PllQDiv::DIV4), // 25mhz / 25 * 192 / 4 = 48Mhz.
@@ -50,6 +54,8 @@ async fn main(_spawner: Spawner) {
         config.rcc.ahb_pre = AHBPrescaler::DIV1;
         config.rcc.apb1_pre = APBPrescaler::DIV2;
         config.rcc.apb2_pre = APBPrescaler::DIV1;
+        // config.rcc.apb1_pre = APBPrescaler::DIV4;
+        // config.rcc.apb2_pre = APBPrescaler::DIV2;
         config.rcc.sys = Sysclk::PLL1_P;
     }
 
