ceac4312bffa1a50d7ad0fd610e903bdd99f8b01
David Ge <davidqge@gmail.com>
2024-08-09 21:00:59 -0500

    Kinesis keymap

--- examples/use_rust/esp32c6_ble/src/main.rs
+++ examples/use_rust/esp32c6_ble/src/main.rs
@@ -26,7 +26,12 @@ fn main() {
     // Pin config
     // WARNING: Some gpio pins shouldn't be used, the initial state is error.
     // reference: table 2-3 in https://www.espressif.com.cn/sites/default/files/documentation/esp32-c3_datasheet_en.pdf
-    let (input_pins, output_pins) = config_matrix_pins_esp!(peripherals: peripherals , input: [gpio6, gpio7, gpio20, gpio21], output: [gpio3, gpio4, gpio5]);
+    let (input_pins, output_pins) = config_matrix_pins_esp!(peripherals: peripherals ,
+    input: [gpio0, gpio1, gpio2, gpio3, gpio4, gpio5, gpio6, gpio7],
+    output: [
+        gpio17, gpio16, gpio15, gpio11, gpio10, gpio8,
+        gpio18, gpio19, gpio20, gpio21, gpio22, gpio23
+    ]);
 
     // Keyboard config
     let vial_config = VialConfig::new(VIAL_KEYBOARD_ID, VIAL_KEYBOARD_DEF);
