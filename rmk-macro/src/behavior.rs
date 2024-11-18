c2a37999d48bc1d39ebdc855eccd24b3b12de2f1
Pedro Casotti <pedro.c.casotti@gmail.com>
2024-11-18 11:23:13 -0300

    feat(one_shot): add behavior option for one shot timeout

--- rmk-macro/src/behavior.rs
+++ rmk-macro/src/behavior.rs
@@ -2,7 +2,7 @@
 //!
 
 use quote::quote;
-use rmk_config::toml_config::TriLayerConfig;
+use rmk_config::toml_config::{OneShotConfig, TriLayerConfig};
 
 use crate::keyboard_config::KeyboardConfig;
 
@@ -18,12 +18,35 @@ fn expand_tri_layer(tri_layer: &Option<TriLayerConfig>) -> proc_macro2::TokenStr
     }
 }
 
+fn expand_one_shot(one_shot: &Option<OneShotConfig>) -> proc_macro2::TokenStream {
+    let default = quote! {::rmk::config::keyboard_config::OneShotConfig::default()};
+    match one_shot {
+        Some(one_shot) => {
+            let millis = match &one_shot.timeout {
+                Some(t) => t.0,
+                None => return default,
+            };
+
+            let timeout = quote! {::embassy_time::Duration::from_millis(#millis)};
+
+            quote! {
+                ::rmk::config::keyboard_config::OneShotConfig {
+                    timeout: #timeout,
+                }
+            }
+        }
+        None => default,
+    }
+}
+
 pub(crate) fn expand_behavior_config(keyboard_config: &KeyboardConfig) -> proc_macro2::TokenStream {
     let tri_layer = expand_tri_layer(&keyboard_config.behavior.tri_layer);
+    let one_shot = expand_one_shot(&keyboard_config.behavior.one_shot);
 
     quote! {
         let behavior_config = ::rmk::config::keyboard_config::BehaviorConfig {
             tri_layer: #tri_layer,
+            one_shot: #one_shot,
         };
     }
 }
