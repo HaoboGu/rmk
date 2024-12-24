4c7a48fcda572b8dde363448baa39ec7af0c1da0
Haobo Gu <haobogu@outlook.com>
2024-12-24 17:40:39 +0800

    feat(input_device): add first POC impl of input device, add rotary encoder as the example
    Signed-off-by: Haobo Gu <haobogu@outlook.com>

--- rmk/src/input_device/mod.rs
+++ rmk/src/input_device/mod.rs
@@ -1,5 +1,9 @@
-//! Input device for RMK
+//! Input device module for RMK
 //!
+//! This module defines the `InputDevice` trait and the `run_devices` macro, enabling the simultaneous execution of multiple input devices.
+//! The `InputDevice` trait provides the interface for individual input devices, and the `run_devices` macro facilitates their concurrent execution.
+//!
+//! Note: The `InputDevice` trait must be used in conjunction with the `run_devices` macro to ensure correct execution of all input devices. 
 
 use core::future::Future;
 
@@ -7,52 +11,67 @@ pub mod rotary_encoder;
 
 /// The trait for input devices.
 ///
-/// This trait MUST be used with `impl_input_device` macro, which will automatically create a static embassy task for the input device.
-/// The following is an example for the usage of this trait:
+/// This trait defines the interface for input devices in RMK.
+/// The `run_devices` macro is required to run tasks associated with input devices concurrently.
+/// 
+/// # Example
 /// ```rust
-/// use core::futures::Future;
+/// // Define an input device
 /// struct MyInputDevice;
+/// 
 /// impl InputDevice for MyInputDevice {
-///     fn run(&mut self) -> impl Future<Output = ()> {
-///         // Implementation
+///     async fn run(&mut self) {
+///         // Input device implementation
 ///     }
 /// }
-/// impl_input_device!(MyInputDevice, my_input_device_task);
-/// ```
-/// The expanded code will be:
-/// ```rust
-/// use core::futures::Future;
-/// struct MyInputDevice;
-/// impl InputDevice for MyInputDevice {
-///     fn run(&mut self) -> impl Future<Output = ()> {
-///         // Implementation
-///     }
-/// }
-/// #[embassy_executor::task]
-/// async fn my_input_device_task(device: MyInputDevice) -> ! {
-///    device.run().await
-///  }
-/// ```
-/// Then, you can spawn `my_input_device_task` as an embassy task:
-/// ```rust
-/// let input_device = MyInputDevice{};
-/// spawner.spawn(my_input_device_task(input_device));
+/// 
+/// // Use the input device
+/// let d1 = MyInputDevice{};
+/// let d2 = MyInputDevice{};
+/// 
+/// // Run all devices simultaneously with RMK
+/// embassy_futures::join::join(
+///     run_devices!(d1, d2),
+///     run_rmk(
+///         // .. arguments
+///     ),
+/// )
+/// .await;
 /// ```
 pub trait InputDevice {
+    /// Starts the input device task.
+    ///
+    /// This asynchronous method should contain the main logic for the input device.
+    /// It will be executed concurrently with other input devices using the `run_devices` macro.
     fn run(&mut self) -> impl Future<Output = ()>;
 }
 
+/// Macro to run multiple input devices concurrently.
+///
+/// The `run_devices` macro is specifically designed to work with the `InputDevice` trait. It takes one or more instances of
+/// input devices and combines their `run` methods into a single future. All futures are executed concurrently, enabling
+/// efficient multitasking for multiple input devices.
+///
+/// # Note
+/// This macro must be used with input devices that implement the `InputDevice` trait.
+///
+/// # Example
+/// ```rust
+/// // `MyInputDevice` should implement `InputDevice` trait
+/// let d1 = MyInputDevice{};
+/// let d2 = MyInputDevice{};
+/// 
+/// // Run all input devices concurrently
+/// run_devices!(d1, d2).await;
+/// ```
 #[macro_export]
-macro_rules! impl_input_device {
-    ($device:ty, $task_name:ident) => {
-        #[::embassy_executor::task]
-        pub async fn $task_name(mut device: $device) -> ! {
-            device.run().await;
-            loop {
-                // If the device.run() completes, we enter an infinite loop to satisfy
-                // the `-> !` return type requirement for embassy tasks
-                ::embassy_time::Timer::after_secs(1).await;
-            }
-        }
+macro_rules! run_devices {
+    // Single device case
+    ($single:expr) => {
+        $single.run()
+    };
+    // Multiple devices case
+    ($first:expr, $second:expr $(, $rest:expr)*) => {
+        ::embassy_futures::join::join($first.run(), run_devices!($second $(, $rest)*))
     };
 }
--- rmk/src/input_device/rotary_encoder.rs
+++ rmk/src/input_device/rotary_encoder.rs
@@ -1,18 +1,159 @@
-use defmt::info;
+//! The rotary encoder implementation is from: https://github.com/leshow/rotary-encoder-hal/blob/master/src/lib.rs
+//!
 
-use crate::impl_input_device;
+use defmt::info;
+use embedded_hal::digital::InputPin;
+#[cfg(feature = "async_matrix")]
+use embedded_hal_async::digital::Wait;
 
 use super::InputDevice;
 
-pub struct RotaryEncoder {}
+/// Holds current/old state and both [`InputPin`](https://docs.rs/embedded-hal/latest/embedded_hal/digital/trait.InputPin.html)
+#[derive(Clone, Debug, Eq, PartialEq)]
+// #[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub struct RotaryEncoder<A, B, P> {
+    pin_a: A,
+    pin_b: B,
+    state: u8,
+    phase: P,
+}
 
-impl InputDevice for RotaryEncoder{
-    async fn run(&mut self) {
-        loop {
-            embassy_time::Timer::after_secs(1).await;
-            info!("hello device")
+/// The encoder direction is either `Clockwise`, `CounterClockwise`, or `None`
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+// #[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub enum Direction {
+    /// A clockwise turn
+    Clockwise,
+    /// A counterclockwise turn
+    CounterClockwise,
+    /// No change
+    None,
+}
+
+/// Allows customizing which Quadrature Phases should be considered movements
+/// and in which direction or ignored.
+pub trait Phase {
+    /// Given the current state `s`, return the direction.
+    fn direction(&mut self, s: u8) -> Direction;
+}
+
+/// Default implementation of `Phase`.
+pub struct DefaultPhase;
+
+/// The useful values of `s` are:
+/// - 0b0001 | 0b0111 | 0b1000 | 0b1110
+/// - 0b0010 | 0b0100 | 0b1011 | 0b1101
+impl Phase for DefaultPhase {
+    fn direction(&mut self, s: u8) -> Direction {
+        match s {
+            0b0001 | 0b0111 | 0b1000 | 0b1110 => Direction::Clockwise,
+            0b0010 | 0b0100 | 0b1011 | 0b1101 => Direction::CounterClockwise,
+            _ => Direction::None,
         }
     }
 }
 
-impl_input_device!(RotaryEncoder, rotary_encoder_task);
+impl<A, B> RotaryEncoder<A, B, DefaultPhase>
+where
+    A: InputPin,
+    B: InputPin,
+{
+    /// Accepts two [`InputPin`](https://docs.rs/embedded-hal/latest/embedded_hal/digital/trait.InputPin.html)s, these will be read on every `update()`
+    pub fn new(pin_a: A, pin_b: B) -> Self {
+        Self {
+            pin_a,
+            pin_b,
+            state: 0u8,
+            phase: DefaultPhase,
+        }
+    }
+}
+
+impl<A: InputPin, B: InputPin, P: Phase> RotaryEncoder<A, B, P> {
+    /// Accepts two [`InputPin`](https://docs.rs/embedded-hal/latest/embedded_hal/digital/trait.InputPin.html)s, these will be read on every `update()`, while using `phase` to determine the direction.
+    pub fn with_phase(pin_a: A, pin_b: B, phase: P) -> Self {
+        Self {
+            pin_a,
+            pin_b,
+            state: 0u8,
+            phase,
+        }
+    }
+
+    /// Call `update` to evaluate the next state of the encoder, propagates errors from `InputPin` read
+    pub fn update(&mut self) -> Direction {
+        // use mask to get previous state value
+        let mut s = self.state & 0b11;
+
+        let (a_is_low, b_is_low) = (self.pin_a.is_low(), self.pin_b.is_low());
+
+        // move in the new state
+        match a_is_low {
+            Ok(true) => s |= 0b0100,
+            Ok(false) => {}
+            Err(_) => return Direction::None,
+        }
+        match b_is_low {
+            Ok(true) => s |= 0b1000,
+            Ok(false) => {}
+            Err(_) => return Direction::None,
+        }
+
+        // move new state in
+        self.state = s >> 2;
+        self.phase.direction(s)
+    }
+
+    /// Returns a reference to the first pin. Can be used to clear interrupt.
+    pub fn pin_a(&mut self) -> &mut A {
+        &mut self.pin_a
+    }
+
+    /// Returns a reference to the second pin. Can be used to clear interrupt.
+    pub fn pin_b(&mut self) -> &mut B {
+        &mut self.pin_b
+    }
+
+    /// Returns a reference to both pins. Can be used to clear interrupt.
+    pub fn pins(&mut self) -> (&mut A, &mut B) {
+        (&mut self.pin_a, &mut self.pin_b)
+    }
+
+    /// Consumes this `Rotary`, returning the underlying pins `A` and `B`.
+    pub fn into_inner(self) -> (A, B) {
+        (self.pin_a, self.pin_b)
+    }
+}
+
+impl<
+        #[cfg(feature = "async_matrix")] A: InputPin + Wait,
+        #[cfg(not(feature = "async_matrix"))] A: InputPin,
+        #[cfg(feature = "async_matrix")] B: InputPin + Wait,
+        #[cfg(not(feature = "async_matrix"))] B: InputPin,
+        P: Phase,
+    > InputDevice for RotaryEncoder<A, B, P>
+{
+    async fn run(&mut self) {
+        loop {
+            #[cfg(feature = "async_matrix")]
+            {
+                let (pin_a, pin_b) = self.pins();
+                embassy_futures::select::select(
+                    pin_a.wait_for_any_edge(),
+                    pin_b.wait_for_any_edge(),
+                )
+                .await;
+            }
+            // If not using async_matrix feature, scanning the encoder pins with 50HZ frequency
+            #[cfg(not(feature = "async_matrix"))]
+            embassy_time::Timer::after_millis(20).await;
+            let direction = self.update();
+            // TODO: Process directions
+            match direction {
+                Direction::Clockwise => info!("Clockwise"),
+                Direction::CounterClockwise => info!("CounterClockwise"),
+                Direction::None => {}
+            }
+        }
+    }
+}
