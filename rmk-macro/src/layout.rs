7a87ca79f0fa60aa7bc9467abfc40ddb997f1ddb
Roberto Previdi <hariseldon78@gmail.com>
2024-12-29 01:09:42 +0100

    use MT for modifier tap/hold

--- rmk-macro/src/layout.rs
+++ rmk-macro/src/layout.rs
@@ -219,8 +219,8 @@ fn parse_key(key: String) -> TokenStream2 {
                 ::rmk::df!(#layer)
             }
         }
-        "MTH" => {
-            if let Some(internal) = key.trim_start_matches("MTH(").strip_suffix(")") {
+        "MT(" => {
+            if let Some(internal) = key.trim_start_matches("MT(").strip_suffix(")") {
                 let keys: Vec<&str> = internal
                     .split_terminator(",")
                     .map(|w| w.trim())
@@ -228,7 +228,7 @@ fn parse_key(key: String) -> TokenStream2 {
                     .collect();
                 if keys.len() != 2 {
                     return quote! {
-                        compile_error!("keyboard.toml: MTH(key, modifier) invalid, please check the documentation: https://haobogu.github.io/rmk/keyboard_configuration.html");
+                        compile_error!("keyboard.toml: MT(key, modifier) invalid, please check the documentation: https://haobogu.github.io/rmk/keyboard_configuration.html");
                     };
                 }
                 let ident = format_ident!("{}", keys[0].to_string());
@@ -236,7 +236,7 @@ fn parse_key(key: String) -> TokenStream2 {
 
                 if (gui || alt || shift || ctrl) == false {
                     return quote! {
-                        compile_error!("keyboard.toml: modifier in MTH(key, modifier) is not valid! Please check the documentation: https://haobogu.github.io/rmk/keyboard_configuration.html");
+                        compile_error!("keyboard.toml: modifier in MT(key, modifier) is not valid! Please check the documentation: https://haobogu.github.io/rmk/keyboard_configuration.html");
                     };
                 }
                 quote! {
@@ -244,7 +244,7 @@ fn parse_key(key: String) -> TokenStream2 {
                 }
             } else {
                 return quote! {
-                    compile_error!("keyboard.toml: MTH(key, modifier) invalid, please check the documentation: https://haobogu.github.io/rmk/keyboard_configuration.html");
+                    compile_error!("keyboard.toml: MT(key, modifier) invalid, please check the documentation: https://haobogu.github.io/rmk/keyboard_configuration.html");
                 };
             }
         }
