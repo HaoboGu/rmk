d9452c95500f72594c287299840378cf2286fcc8
Haobo Gu <haobogu@outlook.com>
2025-02-19 15:21:33 +0800

    refactor: simplify the signal waiting
    Signed-off-by: Haobo Gu <haobogu@outlook.com>

--- rmk/src/ble/led.rs
+++ rmk/src/ble/led.rs
@@ -9,11 +9,8 @@ pub(crate) struct BleLedReader {}
 impl HidReaderTrait for BleLedReader {
     type ReportType = LedIndicator;
 
-    // ESP BLE provides only a blocking callback function for reading data.
-    // So we use a channel to do async read
+    // The LED state is read from a blocking callback, so `Signal` is used to wait for the state.
     async fn read_report(&mut self) -> Result<Self::ReportType, HidError> {
-        let signal = LED_SIGNAL.wait().await;
-        LED_SIGNAL.reset();
-        Ok(signal)
+        Ok(LED_SIGNAL.wait().await)
     }
 }
