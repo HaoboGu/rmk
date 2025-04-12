eca5d89e1b2b206a7a0d4a7e468bf3a9f09936d0
patmuk <patrick@mukherjee.de>
2025-04-12 13:11:58 +0200

    adds comment and clone to MacroOperations

--- rmk/src/keyboard_macro.rs
+++ rmk/src/keyboard_macro.rs
@@ -6,11 +6,12 @@ pub(crate) const MACRO_SPACE_SIZE: usize = 256;
 // Default number of keyboard macros
 pub(crate) const NUM_MACRO: usize = 8;
 
+#[derive(Clone)]
 pub(crate) enum MacroOperation {
     Press(KeyCode),
     Release(KeyCode),
     Tap(KeyCode),
-    Text(KeyCode, bool),
+    Text(KeyCode, bool), // bool = shifted
     Delay(u16),
     End,
 }
