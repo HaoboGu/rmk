1240d435a4ed2b5f4bbdf21f841b05488db4b645
Haobo Gu <haobogu@outlook.com>
2025-04-14 14:04:07 +0800

    test(keymap): fix unit test
    Signed-off-by: Haobo Gu <haobogu@outlook.com>

--- rmk/src/keymap.rs
+++ rmk/src/keymap.rs
@@ -345,15 +345,12 @@ impl<'a, const ROW: usize, const COL: usize, const NUM_LAYER: usize, const NUM_E
 #[cfg(test)]
 mod test {
     use super::{Combo, _reorder_combos};
-    use crate::action::KeyAction;
     use crate::combo::COMBO_MAX_NUM;
     use crate::fork::{Fork, StateBits, FORK_MAX_NUM};
     use crate::hid_state::HidModifiers;
     use crate::k;
     use crate::keymap::_fill_vec;
     use crate::{action::KeyAction, keycode::KeyCode};
-
-    use super::{Combo, _reorder_combos};
 
     #[test]
     fn test_fill_vec() {
