e0fa16dde49e802cb8c2bf9b4db434d137fbff06
Haobo Gu <haobogu@outlook.com>
2025-05-01 23:47:28 +0800

    feat: don't blink when sleep
    Signed-off-by: Haobo Gu <haobogu@outlook.com>

--- rmk/src/ble/trouble/mod.rs
+++ rmk/src/ble/trouble/mod.rs
@@ -237,6 +237,7 @@ pub(crate) async fn run_ble<
                             Either4::Second(Err(BleHostError::BleHost(Error::Timeout))) => {
                                 warn!("Advertising timeout, sleep and wait for any key");
 
+                                BLE_CONNECTION_STATE.store(255, Ordering::Relaxed);
                                 // Set CONNECTION_STATE to true to keep receiving messages from the peripheral
                                 CONNECTION_STATE.store(ConnectionState::Connected.into(), Ordering::Release);
                                 // Wait for the keyboard report for wake the keyboard
