f6420eff1300d300a2ae13a2c54e151120f36e3c
Pedro Casotti <pedro.c.casotti@gmail.com>
2025-05-06 10:02:49 -0300

    feat(controller): separate `EventController` and `PollingController`

--- rmk/src/controller/mod.rs
+++ rmk/src/controller/mod.rs
@@ -3,40 +3,128 @@
 //! This module defines the `Controller` trait and several macros for running output device controllers.
 //! The `Controller` trait provides the interface for individual output device controllers, and the macros facilitate their concurrent execution.
 
-use crate::event::ControllerEvent;
-
 /// The trait for controllers.
 ///
-/// This trait defines the interface for controllers in RMK.
-/// The `run_controllers` macro is required to run tasks associated with controllers concurrently.
+/// This trait defines the interface for event-driven controllers in RMK.
 ///
 /// # Example
 /// ```rust
-/// // Define an input device
-/// struct MyController;
+/// // Define a controller
+/// struct MyController {
+///     // ...
+/// }
 ///
-/// impl Controller for MyController {
-///     async fn process_event(&mut self, event: ControllerEvent) {
-///         // Controller implementation
+/// impl EventController for MyController {
+///     type Event = rmk::event::ControllerEvent;
+///
+///     async fn process_event(&mut self, event: Self::Event) {
+///         // handle event
+///     }
+///     async fn next_message(&mut self) -> Self::Event {
+///         // self.sub.next_message_pure();
 ///     }
 /// }
 ///
 /// // Use the input device
-/// let c1 = MyController{};
-/// let c2 = MyController{};
+/// let c = MyController {
+///     // ...
+/// };
 ///
-/// // Run all devices simultaneously with RMK
+/// // Run device simultaneously with RMK
 /// embassy_futures::join::join(
-///     run_controllers!((c1, c2) => CONTROLLER_CHANNEL),
+///     c.run_controller(),
 ///     run_rmk(
-///         // .. arguments
+///         // ...
 ///     ),
 /// )
 /// .await;
 /// ```
-pub trait Controller {
+pub trait EventController {
+    /// Type of the received events
+    type Event;
+
     /// Process the received event
-    async fn process_event(&mut self, event: ControllerEvent);
+    async fn process_event(&mut self, event: Self::Event);
+
+    /// Block waiting for next message
+    async fn next_message(&mut self) -> Self::Event;
+
+    /// Event loop
+    async fn run_controller(&mut self) {
+        loop {
+            let event = self.next_message().await;
+            self.process_event(event).await;
+        }
+    }
+}
+
+/// The trait for controllers.
+///
+/// This trait defines the interface for polling controllers in RMK.
+///
+/// # Example
+/// ```rust
+/// // Define a controller
+/// struct MyController {
+///     // ...
+/// }
+///
+/// impl PollingController for MyController {
+///     type INTERVAL: embassy_time::Duration = embassy_time::Duration::from_hz(60);
+///     type Event = rmk::event::ControllerEvent;
+///
+///     async fn process_event(&mut self, event: Self::Event) {
+///         // handle event
+///     }
+///     async fn update(&mut self) {
+///         // update periodic
+///     }
+///     fn poll(&mut self) -> Self::Event {
+///         // self.sub.try_next_message_pure();
+///     }
+/// }
+///
+/// // Use the input device
+/// let c = MyController {
+///     // ...
+/// };
+///
+/// // Run device simultaneously with RMK
+/// embassy_futures::join::join(
+///     c.run_polling(),
+///     run_rmk(
+///         // ...
+///     ),
+/// )
+/// .await;
+/// ```
+pub trait PollingController {
+    /// Interval in which `poll` and `update` will be called
+    const INTERVAL: embassy_time::Duration;
+
+    /// Type of the received events
+    type Event;
+
+    /// Process the received event
+    async fn process_event(&mut self, event: Self::Event);
+
+    /// Update periodically
+    async fn update(&mut self);
+
+    /// Wait for next message without blocking
+    fn poll(&mut self) -> Option<Self::Event>;
+
+    /// Polling loop
+    async fn run_polling(&mut self) {
+        loop {
+            if let Some(event) = self.poll() {
+                self.process_event(event).await;
+            }
+            self.update().await;
+
+            embassy_time::Timer::after(Self::INTERVAL).await;
+        }
+    }
 }
 
 /// Macro to bind controllers to event channels and run all of them.
