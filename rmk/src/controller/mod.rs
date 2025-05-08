e8d985ddfe321d4b3b459dcccb39c6c5ffaa50a7
Pedro Casotti <pedro.c.casotti@gmail.com>
2025-05-08 16:04:05 -0300

    feat(controller): create common `Controller` trait

--- rmk/src/controller/mod.rs
+++ rmk/src/controller/mod.rs
@@ -3,54 +3,52 @@
 //! This module defines the `Controller` trait and several macros for running output device controllers.
 //! The `Controller` trait provides the interface for individual output device controllers, and the macros facilitate their concurrent execution.
 
-/// The trait for controllers.
+use embassy_futures::select::{select, Either};
+
+/// Common trait for controllers.
+pub trait Controller {
+    /// Type of the received events
+    type Event;
+
+    /// Block waiting for next message
+    async fn next_message(&mut self) -> Self::Event;
+}
+
+/// The trait for event-driven controllers.
 ///
 /// This trait defines the interface for event-driven controllers in RMK.
 ///
 /// # Example
 /// ```rust
 /// // Define a controller
-/// struct MyController {
-///     // ...
-/// }
+/// struct MyController;
+///
+/// impl Controller for MyController { /* ... */ }
 ///
 /// impl EventController for MyController {
-///     type Event = rmk::event::ControllerEvent;
-///
 ///     async fn process_event(&mut self, event: Self::Event) {
 ///         // handle event
-///     }
-///     async fn next_message(&mut self) -> Self::Event {
-///         // self.sub.next_message_pure();
 ///     }
 /// }
 ///
 /// // Use the input device
-/// let c = MyController {
-///     // ...
-/// };
+/// let c = MyController;
 ///
 /// // Run device simultaneously with RMK
 /// embassy_futures::join::join(
-///     c.run_controller(),
+///     c.event_loop(),
 ///     run_rmk(
 ///         // ...
 ///     ),
 /// )
 /// .await;
 /// ```
-pub trait EventController {
-    /// Type of the received events
-    type Event;
-
+pub trait EventController: Controller {
     /// Process the received event
     async fn process_event(&mut self, event: Self::Event);
 
-    /// Block waiting for next message
-    async fn next_message(&mut self) -> Self::Event;
-
     /// Event loop
-    async fn run_controller(&mut self) {
+    async fn event_loop(&mut self) {
         loop {
             let event = self.next_message().await;
             self.process_event(event).await;
@@ -58,52 +56,44 @@ pub trait EventController {
     }
 }
 
-/// The trait for controllers.
+/// The trait for polling controllers.
 ///
 /// This trait defines the interface for polling controllers in RMK.
 ///
 /// # Example
 /// ```rust
 /// // Define a controller
-/// struct MyController {
-///     // ...
-/// }
+/// struct MyController;
+///
+/// impl Controller for MyController { /* ... */ }
 ///
 /// impl PollingController for MyController {
 ///     type INTERVAL: embassy_time::Duration = embassy_time::Duration::from_hz(60);
-///     type Event = rmk::event::ControllerEvent;
 ///
 ///     async fn process_event(&mut self, event: Self::Event) {
 ///         // handle event
 ///     }
+///
 ///     async fn update(&mut self) {
 ///         // update periodic
-///     }
-///     fn poll(&mut self) -> Self::Event {
-///         // self.sub.try_next_message_pure();
 ///     }
 /// }
 ///
 /// // Use the input device
-/// let c = MyController {
-///     // ...
-/// };
+/// let c = MyController;
 ///
 /// // Run device simultaneously with RMK
 /// embassy_futures::join::join(
-///     c.run_polling(),
+///     c.polling_loop(),
 ///     run_rmk(
 ///         // ...
 ///     ),
 /// )
 /// .await;
 /// ```
-pub trait PollingController {
-    /// Interval in which `poll` and `update` will be called
+pub trait PollingController: Controller {
+    /// Interval between `update` calls
     const INTERVAL: embassy_time::Duration;
-
-    /// Type of the received events
-    type Event;
 
     /// Process the received event
     async fn process_event(&mut self, event: Self::Event);
@@ -111,18 +101,27 @@ pub trait PollingController {
     /// Update periodically
     async fn update(&mut self);
 
-    /// Wait for next message without blocking
-    fn poll(&mut self) -> Option<Self::Event>;
-
     /// Polling loop
-    async fn run_polling(&mut self) {
-        loop {
-            if let Some(event) = self.poll() {
-                self.process_event(event).await;
-            }
-            self.update().await;
+    async fn polling_loop(&mut self) {
+        let mut last = embassy_time::Instant::now();
+        let mut elapsed;
 
-            embassy_time::Timer::after(Self::INTERVAL).await;
+        loop {
+            let now = embassy_time::Instant::now();
+            elapsed = now - last;
+            last = now;
+            match select(
+                embassy_time::Timer::after(Self::INTERVAL - elapsed),
+                self.next_message(),
+            )
+            .await
+            {
+                Either::First(_) => {
+                    self.update().await;
+                    last = embassy_time::Instant::now();
+                }
+                Either::Second(event) => self.process_event(event).await,
+            }
         }
     }
 }
