c4e1a6cead13f52f263b442b9a1f0d4977204108
Menra <112563088+Menra11@users.noreply.github.com>
2025-05-27 21:09:41 +0800

    docs: improve styles and add SCSS support
    - Add custom SCSS stylesheet for enhanced styling
    - Add adaptive features to accommodate pages below 4k
    - Add support for SCSS in the project

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,11 +4,15 @@ import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 import './custom.css'
+import './custom.scss'
+
 export default {
   extends: DefaultTheme,
+  
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      
     })
   },
   enhanceApp({ app, router, siteData }) {
