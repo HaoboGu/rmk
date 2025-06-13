01fb6f881e0b886db682db1eb1a4ef42547951d4
Haobo Gu <haobogu@outlook.com>
2025-06-13 16:02:12 +0800

    feat(script): clean esp examples
    Signed-off-by: Haobo Gu <haobogu@outlook.com>

--- scripts/check_all.sh
+++ scripts/check_all.sh
@@ -1,33 +1,33 @@
-# Compile examples
-cd examples/use_rust/nrf52832_ble && cargo update && cargo build --release && cd ../../..
-cd examples/use_rust/nrf52840 && cargo update && cargo build --release && cd ../../..
-cd examples/use_rust/nrf52840_ble && cargo update && cargo build --release && cd ../../..
-cd examples/use_rust/nrf52840_ble_split && cargo update && cargo build --release && cd ../../..
-cd examples/use_rust/pi_pico_w_ble && cargo update && cargo build --release && cd ../../..
-cd examples/use_rust/pi_pico_w_ble_split && cargo update && cargo build --release && cd ../../..
-cd examples/use_rust/rp2040 && cargo update && cargo build --release && cd ../../..
-cd examples/use_rust/rp2040_direct_pin && cargo update && cargo build --release && cd ../../.. 
-cd examples/use_rust/rp2040_split && cargo update && cargo build --release && cd ../../.. 
-cd examples/use_rust/rp2040_split_pio && cargo update && cargo build --release && cd ../../.. 
-cd examples/use_rust/rp2350 && cargo update && cargo build --release && cd ../../..
-cd examples/use_rust/stm32f1 && cargo update && cargo build --release && cd ../../..
-cd examples/use_rust/stm32f4 && cargo update && cargo build --release && cd ../../..
-cd examples/use_rust/stm32h7 && cargo update && cargo build --release && cd ../../.. 
-cd examples/use_rust/py32f07x && cargo update && cargo build --release && cd ../../.. 
+# # Compile examples
+# cd examples/use_rust/nrf52832_ble && cargo update && cargo build --release && cd ../../..
+# cd examples/use_rust/nrf52840 && cargo update && cargo build --release && cd ../../..
+# cd examples/use_rust/nrf52840_ble && cargo update && cargo build --release && cd ../../..
+# cd examples/use_rust/nrf52840_ble_split && cargo update && cargo build --release && cd ../../..
+# cd examples/use_rust/pi_pico_w_ble && cargo update && cargo build --release && cd ../../..
+# cd examples/use_rust/pi_pico_w_ble_split && cargo update && cargo build --release && cd ../../..
+# cd examples/use_rust/rp2040 && cargo update && cargo build --release && cd ../../..
+# cd examples/use_rust/rp2040_direct_pin && cargo update && cargo build --release && cd ../../.. 
+# cd examples/use_rust/rp2040_split && cargo update && cargo build --release && cd ../../.. 
+# cd examples/use_rust/rp2040_split_pio && cargo update && cargo build --release && cd ../../.. 
+# cd examples/use_rust/rp2350 && cargo update && cargo build --release && cd ../../..
+# cd examples/use_rust/stm32f1 && cargo update && cargo build --release && cd ../../..
+# cd examples/use_rust/stm32f4 && cargo update && cargo build --release && cd ../../..
+# cd examples/use_rust/stm32h7 && cargo update && cargo build --release && cd ../../.. 
+# cd examples/use_rust/py32f07x && cargo update && cargo build --release && cd ../../.. 
 
-cd examples/use_config/nrf52832_ble && cargo update && cargo build --release && cd ../../.. 
-cd examples/use_config/nrf52840_ble && cargo update && cargo build --release && cd ../../.. 
-cd examples/use_config/nrf52840_ble_split && cargo update && cargo build --release --bin central && cargo update && cargo build --release --bin peripheral && cd ../../.. 
-cd examples/use_config/nrf52840_ble_split_direct_pin && cargo update && cargo build --release --bin central && cargo update && cargo build --release --bin peripheral && cd ../../.. 
-cd examples/use_config/pi_pico_w_ble && cargo update && cargo build --release && cd ../../..
-cd examples/use_config/pi_pico_w_ble_split && cargo update && cargo build --release && cd ../../..
-cd examples/use_config/rp2040 && cargo update && cargo build --release && cd ../../.. 
-cd examples/use_config/rp2040_direct_pin && cargo update && cargo build --release && cd ../../.. 
-cd examples/use_config/rp2040_split && cargo update && cargo build --release --bin central && cargo update && cargo build --release --bin peripheral && cd ../../.. 
-cd examples/use_config/rp2040_split_pio && cargo update && cargo build --release --bin central && cargo update && cargo build --release --bin peripheral && cd ../../.. 
-cd examples/use_config/stm32f1 && cargo update && cargo build --release && cd ../../..
-cd examples/use_config/stm32f4 && cargo update && cargo build --release && cd ../../..
-cd examples/use_config/stm32h7 && cargo update && cargo build --release && cd ../../.. 
+# cd examples/use_config/nrf52832_ble && cargo update && cargo build --release && cd ../../.. 
+# cd examples/use_config/nrf52840_ble && cargo update && cargo build --release && cd ../../.. 
+# cd examples/use_config/nrf52840_ble_split && cargo update && cargo build --release --bin central && cargo update && cargo build --release --bin peripheral && cd ../../.. 
+# cd examples/use_config/nrf52840_ble_split_direct_pin && cargo update && cargo build --release --bin central && cargo update && cargo build --release --bin peripheral && cd ../../.. 
+# cd examples/use_config/pi_pico_w_ble && cargo update && cargo build --release && cd ../../..
+# cd examples/use_config/pi_pico_w_ble_split && cargo update && cargo build --release && cd ../../..
+# cd examples/use_config/rp2040 && cargo update && cargo build --release && cd ../../.. 
+# cd examples/use_config/rp2040_direct_pin && cargo update && cargo build --release && cd ../../.. 
+# cd examples/use_config/rp2040_split && cargo update && cargo build --release --bin central && cargo update && cargo build --release --bin peripheral && cd ../../.. 
+# cd examples/use_config/rp2040_split_pio && cargo update && cargo build --release --bin central && cargo update && cargo build --release --bin peripheral && cd ../../.. 
+# cd examples/use_config/stm32f1 && cargo update && cargo build --release && cd ../../..
+# cd examples/use_config/stm32f4 && cargo update && cargo build --release && cd ../../..
+# cd examples/use_config/stm32h7 && cargo update && cargo build --release && cd ../../.. 
 
 cd examples/use_rust/esp32c3_ble && cargo update && cargo build --release && cd ../../..
 cd examples/use_rust/esp32c6_ble && cargo update && cargo build --release && cd ../../..
@@ -63,3 +63,10 @@ cd examples/use_config/rp2040_split_pio && cargo clean && cd ../../..
 cd examples/use_config/stm32f1 && cargo clean && cd ../../..
 cd examples/use_config/stm32f4 && cargo clean && cd ../../..
 cd examples/use_config/stm32h7 && cargo clean && cd ../../..
+
+cd examples/use_config/esp32c3_ble && cargo clean && cd ../../..
+cd examples/use_config/esp32c6_ble && cargo clean && cd ../../..
+cd examples/use_config/esp32s3_ble && cargo clean && cd ../../..
+cd examples/use_rust/esp32c3_ble && cargo clean && cd ../../..
+cd examples/use_rust/esp32c6_ble && cargo clean && cd ../../..
+cd examples/use_rust/esp32s3_ble && cargo clean && cd ../../..
