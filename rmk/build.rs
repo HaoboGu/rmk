fbb86d79d2ba707246fcd8089111f318a1eab6de
Haobo Gu <haobogu@outlook.com>
2025-06-26 19:12:33 +0800

    feat(ble, split): sleep mode after connected
    Signed-off-by: Haobo Gu <haobogu@outlook.com>

--- rmk/build.rs
+++ rmk/build.rs
@@ -64,6 +64,7 @@ fn get_constants_str(constants: RmkConstantsConfig) -> String {
         const_declaration!(pub(crate) SPLIT_PERIPHERALS_NUM = constants.split_peripherals_num),
         const_declaration!(pub(crate) SPLIT_MESSAGE_CHANNEL_SIZE = constants.split_message_channel_size),
         const_declaration!(pub(crate) NUM_BLE_PROFILE = constants.ble_profiles_num),
+        const_declaration!(pub(crate) SPLIT_CENTRAL_SLEEP_TIMEOUT_MINUTES = constants.split_central_sleep_timeout_minutes),
         format!("pub(crate) const BUILD_HASH: u32 = {:#010x};\n", build_hash),
     ]
     .map(|s| "#[allow(clippy::redundant_static_lifetimes)]\n".to_owned() + s.as_str())
