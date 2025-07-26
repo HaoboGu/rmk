1c3c36018d39fe4de941cf5e5a8a9f947a92ad58
hitsmaxft <mfthits@gmail.com>
2025-07-26 08:45:54 +0800

    feat: ble.default_tx_power option for improve connection with nrf

--- rmk-macro/src/split/peripheral.rs
+++ rmk-macro/src/split/peripheral.rs
@@ -61,6 +61,12 @@ pub(crate) fn parse_split_peripheral_mod(id: usize, _attr: proc_macro::TokenStre
 fn expand_bind_interrupt_for_split_peripheral(chip: &ChipModel, communication: &CommunicationConfig) -> TokenStream2 {
     match chip.series {
         ChipSeries::Nrf52 => {
+            let tx_power = if let Some(pwr) = communication.get_ble_config().unwrap().default_tx_power {
+                quote! { .default_tx_power(#pwr)?  }
+            } else {
+                quote! {}
+            };
+
             quote! {
                 use ::embassy_nrf::bind_interrupts;
                 bind_interrupts!(struct Irqs {
@@ -98,6 +104,7 @@ fn expand_bind_interrupt_for_split_peripheral(chip: &ChipModel, communication: &
                         .support_phy_update_central()?
                         .support_phy_update_peripheral()?
                         .support_le_2m_phy()?
+                        #tx_power
                         .peripheral_count(1)?
                         .buffer_cfg(L2CAP_MTU as u16, L2CAP_MTU as u16, L2CAP_TXQ, L2CAP_RXQ)?
                         .build(p, rng, mpsl, mem)
