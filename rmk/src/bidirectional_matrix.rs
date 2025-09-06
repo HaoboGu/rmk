f7320099c8072388059abe74d6698fc3459731c8
archnode <markus@aichinger.dev>
2025-09-06 15:43:40 +0200

    #406: Cleanup bidirectional matrix.

--- rmk/src/bidirectional_matrix.rs
+++ rmk/src/bidirectional_matrix.rs
@@ -69,13 +69,16 @@ impl<
 {
     async fn read_event(&mut self) -> crate::event::Event {
         loop {
-            let (mut out_idx_start, mut in_idx_start) = self.scan_pos;
+            let (out_idx_start, in_idx_start) = self.scan_pos;
             #[cfg(feature = "async_matrix")]
             self.wait_for_key().await;
             
+            // Scan pos smaller than number of pins: Output pins = output mode, input pins = input mode
+            // Scan output pins to input pins
             if out_idx_start < OUTPUT_PIN_NUM {                
-                // Scan matrix and send report
                 for out_idx in out_idx_start..self.output_pins.len() {
+                    let col_idx = out_idx * 2;
+                    let scan_out_idx = out_idx;
                     // Pull up output pin, wait 1us ensuring the change comes into effect
                     if let Some(out_pin) = self.output_pins.get_mut(out_idx) {
                         out_pin.set_high().ok();
@@ -87,78 +90,84 @@ impl<
                         // Check input pins and debounce
                         let debounce_state = self.debouncer.detect_change_with_debounce(
                             in_idx,
-                            out_idx,
+                            col_idx,
                             in_pin.is_high().ok().unwrap_or_default(),
-                            &self.key_state[out_idx][in_idx],
+                            &self.key_state[col_idx][in_idx],
                         );
     
                         if let DebounceState::Debounced = debounce_state {
-                            self.key_state[out_idx][in_idx].toggle_pressed();
-    
-                            self.scan_pos = (out_idx, in_idx);
-                            return Event::Key(KeyboardEvent::key(in_idx as u8, (out_idx * 2) as u8, self.key_state[out_idx][in_idx].pressed));
+                            self.key_state[col_idx][in_idx].toggle_pressed();
+                            self.scan_pos = (scan_out_idx, in_idx);
+                            // We alter the output index to have a more logical matrix output for regular matrix layouts.
+                            return Event::Key(KeyboardEvent::key(in_idx as u8, col_idx as u8, self.key_state[col_idx][in_idx].pressed));
                         }
     
                         // If there's key still pressed, always refresh the self.scan_start
                         #[cfg(feature = "async_matrix")]
-                        if self.key_state[out_idx][in_idx].pressed {
+                        if self.key_state[col_idx][in_idx].pressed {
                             self.scan_start = Some(Instant::now());
                         }
                     }
-    
                     // Pull it back to low
                     if let Some(out_pin) = self.output_pins.get_mut(out_idx) {
                         out_pin.set_low().ok();
                     }
                 }
-                out_idx_start = OUTPUT_PIN_NUM;
-                in_idx_start = 0;
             }
 
-            // Set all output pins back to low
-            for out in self.output_pins.iter_mut() {
-                out.set_low().ok();
+            // Set all output pins back to low and make input.
+            for output in self.output_pins.iter_mut() {
+                output.set_as_input();
             }
-            
-            let local_in_idx_start = out_idx_start - OUTPUT_PIN_NUM;
-            let local_out_idx_start = in_idx_start;
+            for input in self.input_pins.iter_mut() {
+                input.set_as_output();
+            }
+            // Transform scan pos index to a local loop index.
+            let out_idx_start = if out_idx_start >= OUTPUT_PIN_NUM { out_idx_start - OUTPUT_PIN_NUM } else { 0 };
             // Scan matrix in reverse and send report
-            for in_idx in local_in_idx_start..self.input_pins.len() {
+            for out_idx in out_idx_start..self.input_pins.len() {
+                let scan_out_idx = out_idx + OUTPUT_PIN_NUM;
                 // Pull up output pin, wait 1us ensuring the change comes into effect
-                if let Some(in_pin) = self.input_pins.get_mut(in_idx) {
-                    in_pin.set_high().ok();
+                if let Some(out_pin) = self.input_pins.get_mut(out_idx) {
+                    out_pin.set_high().ok();
                 }
                 Timer::after_micros(1).await;
                 // Only use start index for the first iteration.
-                let out_idx_start_current = if in_idx == local_in_idx_start { local_out_idx_start } else { 0 };
-                for out_idx in out_idx_start_current..self.output_pins.len() {
-                    let col_idx = out_idx + OUTPUT_PIN_NUM;
-                    let out_pin = self.input_pins.get_mut(out_idx).unwrap();
+                let in_idx_start_current = if out_idx == out_idx_start { in_idx_start } else { 0 };
+                for in_idx in in_idx_start_current..self.output_pins.len() {
+                    let col_idx = in_idx * 2 + 1;
+                    let in_pin = self.output_pins.get_mut(in_idx).unwrap();
                     // Check input pins and debounce
                     let debounce_state = self.debouncer.detect_change_with_debounce(
-                        col_idx,
                         in_idx,
-                        out_pin.is_high().ok().unwrap_or_default(),
-                        &self.key_state[col_idx][in_idx],
+                        scan_out_idx,
+                        in_pin.is_high().ok().unwrap_or_default(),
+                        &self.key_state[col_idx][out_idx],
                     );
 
                     if let DebounceState::Debounced = debounce_state {
-                        self.key_state[col_idx][in_idx].toggle_pressed();
-                        self.scan_pos = (in_idx, col_idx);
-                        return Event::Key(KeyboardEvent::key(in_idx as u8, (out_idx * 2 + 1) as u8, self.key_state[col_idx][in_idx].pressed));
+                        self.key_state[col_idx][out_idx].toggle_pressed();
+                        self.scan_pos = (scan_out_idx, in_idx);
+                        return Event::Key(KeyboardEvent::key(out_idx as u8, col_idx as u8, self.key_state[col_idx][out_idx].pressed));
                     }
 
                     // If there's key still pressed, always refresh the self.scan_start
                     #[cfg(feature = "async_matrix")]
-                    if self.key_state[out_idx][in_idx].pressed {
+                    if self.key_state[col_idx][out_idx].pressed {
                         self.scan_start = Some(Instant::now());
                     }
                 }
-
                 // Pull it back to low
-                if let Some(in_pin) = self.output_pins.get_mut(in_idx) {
-                    in_pin.set_low().ok();
+                if let Some(out_idx) = self.input_pins.get_mut(out_idx) {
+                    out_idx.set_low().ok();
                 }
+            }
+            // Set input pins back to input.
+            for input in self.input_pins.iter_mut() {
+                input.set_as_input();
+            }
+            for output in self.output_pins.iter_mut() {
+                output.set_as_output();
             }
             self.scan_pos = (0, 0);
         }
@@ -178,37 +187,4 @@ impl<
 {
     const ROW: usize = INPUT_PIN_NUM;
     const COL: usize = COLS;
-
-    #[cfg(feature = "async_matrix")]
-    // @TODO: Update for bidirectional scanning.
-    async fn wait_for_key(&mut self) {
-        use core::pin::pin;
-
-        if let Some(start_time) = self.scan_start {
-            // If no key press over 1ms, stop scanning and wait for interupt
-            if start_time.elapsed().as_millis() <= 1 {
-                return;
-            } else {
-                self.scan_start = None;
-            }
-        }
-        // First, set all output pin to high
-        for out in self.output_pins.iter_mut() {
-            out.set_high().ok();
-        }
-        Timer::after_micros(1).await;
-        let mut futs: Vec<_, INPUT_PIN_NUM> = self
-            .input_pins
-            .iter_mut()
-            .map(|input_pin| input_pin.wait_for_high())
-            .collect();
-        let _ = select_slice(pin!(futs.as_mut_slice())).await;
-
-        // Set all output pins back to low
-        for out in self.output_pins.iter_mut() {
-            out.set_low().ok();
-        }
-
-        self.scan_start = Some(Instant::now());
-    }
 }
--- rmk/src/driver/mod.rs
+++ rmk/src/driver/mod.rs
@@ -1,3 +1,4 @@
 /// Driver module containing the common drivers for the keyboard
 pub mod gpio;
+#[cfg(feature = "bidirectional")]
 pub mod flex_pin;
