7cd1417f556b39b5ca2d69ea7a6debd1df21a91e
Haobo Gu <haobogu@outlook.com>
2025-09-16 19:12:09 +0800

    fix(example): fix examples build error when vial feature is disabled
    Signed-off-by: Haobo Gu <haobogu@outlook.com>

--- examples/use_rust/sf32lb52x_usb/src/main.rs
+++ examples/use_rust/sf32lb52x_usb/src/main.rs
@@ -65,12 +65,12 @@ async fn main(_spawner: Spawner) {
         serial_number: "vial:f64c2b3c:000001",
     };
 
-    let vial_config = VialConfig::new(VIAL_KEYBOARD_ID, VIAL_KEYBOARD_DEF, &[(0, 0), (1, 1)]);
+    let _vial_config = VialConfig::new(VIAL_KEYBOARD_ID, VIAL_KEYBOARD_DEF, &[(0, 0), (1, 1)]);
     // let storage_config = rmk::config::StorageConfig::default();
 
     let rmk_config = RmkConfig {
         usb_config: keyboard_usb_config,
-        vial_config,
+        // vial_config,
         ..Default::default()
     };
 
@@ -101,7 +101,7 @@ async fn main(_spawner: Spawner) {
         run_devices!((matrix) => EVENT_CHANNEL),
         keyboard.run(),
         // run_rmk(&keymap, driver, &mut storage, rmk_config),
-        run_rmk(&keymap, driver, rmk_config),
+        run_rmk(driver, rmk_config),
     )
     .await;
 }
