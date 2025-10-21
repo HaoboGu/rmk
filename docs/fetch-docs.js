35d0cb67cb18de9f704d476010fd316d0ce87c63
liyang <272891021@qq.com>
2025-10-21 16:22:52 +0800

    fix: fetch-docs on windows

--- docs/fetch-docs.js
+++ docs/fetch-docs.js
@@ -1,15 +1,12 @@
 import { execSync } from 'child_process'
 import { readFileSync, mkdirSync, existsSync } from 'fs'
-import { join } from 'path'
 
 const versions = JSON.parse(readFileSync(new URL('./versions.json', import.meta.url)))
-
 const docsRoot = 'docs'
 
 versions.forEach((branch) => {
   const version = branch.split('/').pop()
-  
-  const targetDir = join(docsRoot, version)
+  const targetDir = docsRoot + '/' + version
 
   if (!existsSync(targetDir)) {
     mkdirSync(targetDir, { recursive: true }) 
@@ -17,9 +14,9 @@ versions.forEach((branch) => {
 
   // Fetch origin branch first
   execSync(`git fetch origin ${branch}`, { stdio: 'inherit', shell: 'bash' });
-  
-  console.log(`Fetched branch ${branch} into ${targetDir}`)
-
+  // Extract branch
   const command = `git archive origin/${branch} docs/main | tar -x -C ${targetDir} --strip-components=2`
   execSync(command, { stdio: 'inherit', shell: 'bash' })
+
+  console.log(`Fetched branch ${branch} into ${targetDir}`)
 })
