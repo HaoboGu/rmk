28c9c1dacc30bf162e0f802a05d7d25f5a95c474
Haobo Gu <haobogu@outlook.com>
2025-10-21 10:43:56 +0800

    docs: fix docs build
    Signed-off-by: Haobo Gu <haobogu@outlook.com>

--- docs/fetch-docs.js
+++ docs/fetch-docs.js
@@ -1,11 +1,22 @@
 import { execSync } from 'child_process'
-import { readFileSync } from 'fs'
+import { readFileSync, mkdirSync, existsSync } from 'fs'
+import { join } from 'path'
 
 const versions = JSON.parse(readFileSync(new URL('./versions.json', import.meta.url)))
 
+const docsRoot = 'docs/docs'
+
 versions.forEach((branch) => {
   const version = branch.split('/').pop()
-  const command = `git archive ${branch} docs/main | tar -x -C docs/ --transform "s|docs/main|${version}|"`
-  console.log(`Fetched docs ${branch}`)
+  
+  const targetDir = join(docsRoot, version)
+
+  if (!existsSync(targetDir)) {
+    mkdirSync(targetDir, { recursive: true }) 
+  }
+
+  const command = `git archive origin/${branch} docs/main | tar -x -C ${targetDir} --strip-components=2`
+  
+  console.log(`Fetched branch ${branch} into ${targetDir}`)
   execSync(command, { stdio: 'inherit', shell: 'bash' })
 })
