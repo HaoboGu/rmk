61ba066448619619dc3e1190776653ea0d7e240f
LiYang <63987506+liyang8246@users.noreply.github.com>
2025-10-21 19:31:55 +0800

    docs: redirect 404 page to home page and some bugs fixed (#591)
    * fix: fetch-docs on windows
    
    * feat(docs): add custom not found
    
    * fmt
    
    * docs(main): update guide link path in _meta.json
    
    * docs(main): fix logo image path in main index

--- docs/fetch-docs.js
+++ docs/fetch-docs.js
@@ -1,25 +1,22 @@
 import { execSync } from 'child_process'
 import { readFileSync, mkdirSync, existsSync } from 'fs'
-import { join } from 'path'
 
 const versions = JSON.parse(readFileSync(new URL('./versions.json', import.meta.url)))
-
 const docsRoot = 'docs'
 
 versions.forEach((branch) => {
   const version = branch.split('/').pop()
-  
-  const targetDir = join(docsRoot, version)
+  const targetDir = docsRoot + '/' + version
 
   if (!existsSync(targetDir)) {
-    mkdirSync(targetDir, { recursive: true }) 
+    mkdirSync(targetDir, { recursive: true })
   }
 
   // Fetch origin branch first
-  execSync(`git fetch origin ${branch}`, { stdio: 'inherit', shell: 'bash' });
-  
-  console.log(`Fetched branch ${branch} into ${targetDir}`)
-
+  execSync(`git fetch origin ${branch}`, { stdio: 'inherit', shell: 'bash' })
+  // Extract branch
   const command = `git archive origin/${branch} docs/main | tar -x -C ${targetDir} --strip-components=2`
   execSync(command, { stdio: 'inherit', shell: 'bash' })
+
+  console.log(`Fetched branch ${branch} into ${targetDir}`)
 })
