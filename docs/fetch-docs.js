7587512234b5247c6fe707ca0264d28265dd8786
Haobo Gu <haobogu@outlook.com>
2025-10-21 11:17:09 +0800

    try fix
    Signed-off-by: Haobo Gu <haobogu@outlook.com>

--- docs/fetch-docs.js
+++ docs/fetch-docs.js
@@ -17,7 +17,7 @@ versions.forEach((branch) => {
 
   // Fetch origin branch first
   execSync(`git fetch origin ${branch}`, { stdio: 'inherit', shell: 'bash' });
-  const command = `git archive origin/${branch} docs/docs/main | tar -x -C ${targetDir} --strip-components=3`
+  const command = `git archive origin/${branch} docs/main | tar -x -C ${targetDir} --strip-components=3`
   
   console.log(`Fetched branch ${branch} into ${targetDir}`)
   execSync(command, { stdio: 'inherit', shell: 'bash' })
