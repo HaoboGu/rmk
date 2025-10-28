60b7fe57612c66f8acf7d8e0073ce682fa58ccd2
liyang <272891021@qq.com>
2025-10-28 11:16:01 +0800

    feat(docs): add dark mode support for language badges

--- docs/docs/main/components/LangBadge.tsx
+++ docs/docs/main/components/LangBadge.tsx
@@ -1,12 +1,20 @@
-const langBadge = (name: string, icon: string) => () => (
-  <div style={{ display: 'flex', alignItems: 'center', gap: '0.3rem' }}>
-    <span className={icon} />
-    <span>{name}</span>
-  </div>
-)
+import { useDark } from 'rspress/runtime'
+
+const langBadge = (name: string, defaultIcon: string, darkIcon?: string) => () => {
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', gap: '0.3rem' }}>
+      <span className={useDark() && darkIcon ? darkIcon : defaultIcon} />
+      <span>{name}</span>
+    </div>
+  )
+}
 
 // https://icon-sets.iconify.design/material-icon-theme/
 export const Rust = langBadge('Rust', 'icon-[material-icon-theme--rust]')
-export const Toml = langBadge('Toml', 'icon-[material-icon-theme--toml]')
+export const Toml = langBadge(
+  'Toml',
+  'icon-[material-icon-theme--toml-light]',
+  'icon-[material-icon-theme--toml]'
+)
 
-export default () => null;
+export default () => null
