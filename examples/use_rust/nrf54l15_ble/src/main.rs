72e775b900f6845f05e765581d2e15eb0fa717d2
Haobo Gu <haobogu@outlook.com>
2025-11-05 23:29:06 +0800

    refactor: update nrf54 example
    Signed-off-by: Haobo Gu <haobogu@outlook.com>

--- examples/use_rust/nrf54l15_ble/src/main.rs
+++ examples/use_rust/nrf54l15_ble/src/main.rs
@@ -10,11 +10,8 @@ use defmt::{info, unwrap};
 use embassy_executor::Spawner;
 use embassy_nrf::cracen;
 use embassy_nrf::gpio::{Input, Output};
-use embassy_nrf::interrupt::{self, InterruptExt};
 use embassy_nrf::mode::Blocking;
-use embassy_nrf::peripherals::SAADC;
-use embassy_nrf::saadc::{self, AnyInput, Input as _, Saadc};
-use embassy_nrf::{Peri, bind_interrupts, pac};
+use embassy_nrf::{bind_interrupts, pac};
 use keymap::{COL, ROW};
 // use nrf_mpsl::Flash;
 use crate::vial::{VIAL_KEYBOARD_DEF, VIAL_KEYBOARD_ID};
@@ -24,30 +21,24 @@ use rand_chacha::ChaCha12Rng;
 use rand_core::SeedableRng;
 use rmk::ble::build_ble_stack;
 use rmk::channel::EVENT_CHANNEL;
-use rmk::config::{
-    BehaviorConfig, BleBatteryConfig, DeviceConfig, PositionalConfig, RmkConfig, StorageConfig, VialConfig,
-};
+use rmk::config::{BehaviorConfig, DeviceConfig, PositionalConfig, RmkConfig, VialConfig};
 use rmk::debounce::default_debouncer::DefaultDebouncer;
 use rmk::futures::future::join4;
 use rmk::input_device::Runnable;
-use rmk::input_device::adc::{AnalogEventType, NrfAdc};
 use rmk::input_device::battery::BatteryProcessor;
-use rmk::input_device::rotary_encoder::{DefaultPhase, RotaryEncoder};
 use rmk::keyboard::Keyboard;
 use rmk::matrix::Matrix;
-use rmk::storage::dummy_flash::DummyFlash;
-use rmk::{HostResources, initialize_encoder_keymap_and_storage, run_devices, run_processor_chain, run_rmk};
+use rmk::{HostResources, initialize_encoder_keymap, run_devices, run_processor_chain, run_rmk};
 use static_cell::StaticCell;
 
 use {defmt_rtt as _, panic_probe as _};
 
 bind_interrupts!(struct Irqs {
-    SWI00 => mpsl::LowPrioInterruptHandler;
+    SWI00 => nrf_sdc::mpsl::LowPrioInterruptHandler;
     CLOCK_POWER => nrf_sdc::mpsl::ClockInterruptHandler;
-    RADIO_0 => mpsl::HighPrioInterruptHandler;
-    TIMER10 => mpsl::HighPrioInterruptHandler;
-    SAADC => saadc::InterruptHandler;
-    GRTC_3 => mpsl::HighPrioInterruptHandler;
+    RADIO_0 => nrf_sdc::mpsl::HighPrioInterruptHandler;
+    TIMER10 => nrf_sdc::mpsl::HighPrioInterruptHandler;
+    GRTC_3 => nrf_sdc::mpsl::HighPrioInterruptHandler;
 });
 
 #[embassy_executor::task]
@@ -81,16 +72,6 @@ fn build_sdc<'d, const N: usize>(
         .peripheral_count(1)?
         .buffer_cfg(L2CAP_MTU as u16, L2CAP_MTU as u16, L2CAP_TXQ, L2CAP_RXQ)?
         .build(p, rng, mpsl, mem)
-}
-
-/// Initializes the SAADC peripheral in single-ended mode on the given pin.
-fn init_adc(adc_pin: AnyInput, adc: Peri<'static, SAADC>) -> Saadc<'static, 1> {
-    // Then we initialize the ADC. We are only using one channel in this example.
-    let config = saadc::Config::default();
-    let channel_cfg = saadc::ChannelConfig::single_ended(adc_pin.degrade_saadc());
-    interrupt::SAADC.set_priority(interrupt::Priority::P3);
-    let saadc = saadc::Saadc::new(adc, Irqs, config, [channel_cfg]);
-    saadc
 }
 
 fn ble_addr() -> [u8; 6] {
@@ -139,6 +120,7 @@ async fn main(spawner: Spawner) {
         SESSION_MEM.init(mpsl::SessionMem::new())
     )));
     spawner.spawn(mpsl_task(&*mpsl).unwrap());
+    info!("mpsl task spawned");
     let sdc_p = sdc::Peripherals::new(
         p.PPI00_CH1,
         p.PPI00_CH3,
@@ -162,47 +144,31 @@ async fn main(spawner: Spawner) {
     );
     let mut rng = cracen::Cracen::new_blocking(p.CRACEN);
     let mut rng_gen = ChaCha12Rng::from_rng(&mut rng).unwrap();
-    let mut sdc_mem = sdc::Mem::<4096>::new();
+    let mut sdc_mem = sdc::Mem::<6144>::new();
     let sdc = unwrap!(build_sdc(sdc_p, &mut rng, mpsl, &mut sdc_mem));
+    info!("sdc built");
     let mut host_resources = HostResources::new();
     let stack = build_ble_stack(sdc, ble_addr(), &mut rng_gen, &mut host_resources).await;
+    info!("stack built");
 
     // Initialize flash
     // let flash = Flash::take(mpsl, p.NVMC);
-    let flash = DummyFlash::new();
 
     // Initialize IO Pins
     let (row_pins, col_pins) = config_matrix_pins_nrf!(peripherals: p, input: [P0_00, P0_01, P0_02, P0_03, P0_04], output:  [P0_05, P0_06, P1_00, P1_01, P1_02, P1_03, P1_04, P1_05, P1_06, P1_07, P1_08, P1_09, P1_10, P1_11]);
-
-    // Initialize the ADC.
-    // We are only using one channel for detecting battery level
-    let adc_pin = p.P1_14.degrade_saadc();
-    let is_charging_pin = Input::new(p.P2_09, embassy_nrf::gpio::Pull::Up);
-    let saadc = init_adc(adc_pin, p.SAADC);
-    // Wait for ADC calibration.
-    saadc.calibrate().await;
 
     // Keyboard config
     let keyboard_device_config = DeviceConfig {
         vid: 0x4c4b,
         pid: 0x4643,
         manufacturer: "Haobo",
-        product_name: "RMK Keyboard",
+        product_name: "RMK nRF54",
         serial_number: "vial:f64c2b3c:000001",
     };
 
     let _vial_config = VialConfig::new(VIAL_KEYBOARD_ID, VIAL_KEYBOARD_DEF, UNLOCK_KEYS);
-    let ble_battery_config = BleBatteryConfig::new(Some(is_charging_pin), true, None, false);
-    let storage_config = StorageConfig {
-        start_addr: 0xA0000, // FIXME: use 0x70000 after we can build without softdevice controller
-        num_sectors: 6,
-        ..Default::default()
-    };
     let rmk_config = RmkConfig {
         device_config: keyboard_device_config,
-        // vial_config,
-        ble_battery_config,
-        storage_config,
         ..Default::default()
     };
 
@@ -212,11 +178,9 @@ async fn main(spawner: Spawner) {
     let mut key_config = PositionalConfig::default();
     let mut behavior_config = BehaviorConfig::default();
     let mut encoder_map = keymap::get_default_encoder_map();
-    let (keymap, mut storage) = initialize_encoder_keymap_and_storage(
+    let keymap = initialize_encoder_keymap(
         &mut default_keymap,
         &mut encoder_map,
-        flash,
-        &storage_config,
         &mut behavior_config,
         &mut key_config,
     )
@@ -228,28 +192,17 @@ async fn main(spawner: Spawner) {
     // let mut matrix = TestMatrix::<ROW, COL>::new();
     let mut keyboard = Keyboard::new(&keymap);
 
-    // Initialize the encoder
-    let pin_a = Input::new(p.P2_06, embassy_nrf::gpio::Pull::None);
-    let pin_b = Input::new(p.P2_04, embassy_nrf::gpio::Pull::None);
-    let mut encoder = RotaryEncoder::with_phase(pin_a, pin_b, DefaultPhase, 0);
-
-    let mut adc_device = NrfAdc::new(
-        saadc,
-        [AnalogEventType::Battery],
-        embassy_time::Duration::from_secs(12),
-        None,
-    );
     let mut batt_proc = BatteryProcessor::new(2000, 2806, &keymap);
 
     join4(
         run_devices! (
-            (matrix, encoder, adc_device) => EVENT_CHANNEL,
+            (matrix) => EVENT_CHANNEL,
         ),
         run_processor_chain! {
             EVENT_CHANNEL => [batt_proc],
         },
         keyboard.run(), // Keyboard is special
-        run_rmk(&stack, &mut storage, rmk_config),
+        run_rmk(&stack, rmk_config),
     )
     .await;
 }
