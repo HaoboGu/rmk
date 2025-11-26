f3293bab9ff093faa0d8af257d48865f659c2f06
adong660 <adong660@outlook.com>
2025-11-26 19:19:07 +0800

    refactor: make the logic of Caps Word clearer
    Define a state machine enum to simplify the logic.

--- rmk-types/src/keycode.rs
+++ rmk-types/src/keycode.rs
@@ -1024,7 +1024,7 @@ impl KeyCode {
         }
     }
 
-    /// Does current keycode continues caps word?
+    /// Does current keycode continues Caps Word?
     pub fn is_caps_word_continue_key(self) -> bool {
         if self >= KeyCode::A && self <= KeyCode::Z {
             return true;
@@ -1038,7 +1038,7 @@ impl KeyCode {
         false
     }
 
-    /// Does current keycode is to be shifted by caps word?
+    /// Does current keycode is to be shifted by Caps Word?
     pub fn is_caps_word_shifted_key(self) -> bool {
         if self >= KeyCode::A && self <= KeyCode::Z {
             return true;
